a3aa45aaca98d2cd5dc8f00b1d4d62896e8c5e78
FERNANDA TEIXEIRA NOGUEIRA LISBOA <fernanda@poppymidia.com.br>
2023-08-10 13:25:41 -0300

    slide of photo films

--- src/app/pages/home/components/components.component.ts
+++ src/app/pages/home/components/components.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-components',
   templateUrl: './components.component.html',
   styleUrls: ['./components.component.css']
 })
-export class ComponentsComponent {
+
+
+export class ComponentsComponent  implements OnInit{
+
+ styleActive:string = ""
+  image:string =""
+  title:string=""
+
+  filmes:any[]= [
+    
+    {"name": 'My Adventureswith superman', "image": "superman.avif"},
+     {"name": 'Gossip Girl (2007)', "image": "gosip.avif"},
+     {"name": 'winning time', "image": "winning_time.avif"},
+     {"name": 'full cricle', "image": "fullcircle.avif"},
+     {"name": 'and just like that', "image": "justlikethat.avif"},
+     {"name": 'alem do guarda-roupas', "image": "alemdo.avif"},
+     {"name": 'and just like that', "image": "justlikethat.avif"},
+     {"name": 'alem do guarda-roupas', "image": "alemdo.avif"}
+
+ 
+
+    
+    ]
+    carousel:any[]= []
+
+   
+  
+
+
+    constructor(){}
+    separateImages(){
+      let active:string=""
+    
+      for(let i=0; i <= this.filmes.length; i+=3){
+        console.log('filmes',this.filmes)
+        this.carousel.push([this.filmes.splice(0,3)])
+        console.log('filmes',this.filmes)
+        console.log('carousel', this.carousel)
+
+      }
+    
+     
+     
+    }
+
+  
+  
+  ngOnInit(): void {
+
+    
+
+
+    
+  this.separateImages()
+
+  if(this.carousel[0]){
+    this.styleActive="active"
+    console.log('style',this.styleActive)
+  }else{
+    this.styleActive=""
+    console.log('style',this.styleActive)
+  }
+
+  
+ 
+  }
+  
 
 }
